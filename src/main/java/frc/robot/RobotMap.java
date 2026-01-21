aa2fa96d7c42cf88ec212d98165ec2d517859afd
LukeFN8 <gil.tanny@gmail.com>
2026-01-21 21:43:05 +0200

    RE-made ShooterSystem and added some PID settings to the leader motor

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,7 +7,11 @@ public class RobotMap {
     // add constants here
     // public static final type NAME = value;
     //Shooting
-    public static final int LEFT_SHOOTER_MOTOR = 1;
-    public static final int RIGHT_SHOOTER_MOTOR = 2;
+    public static final int MAIN_SHOOTER_MOTOR = 1;
+    public static final int FOLLOWING_SHOOTER_MOTOR = 2;
     public static final int PITCH_MOTOR= 3;
+    public static final int KP = 4;
+    public static final int KI = 4;
+    public static final int KD = 4;
+    public static final int FF = 3;
 }
--- src/main/java/frc/robot/subsystems/ShooterSystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSystem.java
@@ -1,32 +1,50 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class ShooterSystem extends SubsystemBase {
-    private final SparkMax shooterMotorleft ;
-    private final SparkMax shooterMotorright;
-    private final Servo pitch;
+    private final SparkMax shooterMotorMain ;
+    private final RelativeEncoder speedEncoder;
+    private final SparkMax shooterMotorFollow;
+    private final Servo pitchMotor;
+
+    public final SparkClosedLoopController sparkPIDcontroller;
 
     public ShooterSystem(){
-        shooterMotorleft=new SparkMax(RobotMap.LEFT_SHOOTER_MOTOR, SparkLowLevel.MotorType.kBrushless);
-        shooterMotorright=new SparkMax(RobotMap.RIGHT_SHOOTER_MOTOR,SparkLowLevel.MotorType.kBrushless);
-        pitch=new Servo(RobotMap.PITCH_MOTOR);
+        shooterMotorMain=new SparkMax(RobotMap.MAIN_SHOOTER_MOTOR, SparkLowLevel.MotorType.kBrushless);
+        shooterMotorFollow=new SparkMax(RobotMap.FOLLOWING_SHOOTER_MOTOR, SparkLowLevel.MotorType.kBrushless);
+        pitchMotor=new Servo(RobotMap.PITCH_MOTOR);
+
+        sparkPIDcontroller = shooterMotorMain.getClosedLoopController();
+
         SparkMaxConfig config_lead= new SparkMaxConfig();
-        SparkMaxConfig config_folllow= new SparkMaxConfig();
-        config_folllow.follow(shooterMotorleft, true);
-        shooterMotorleft.configure(config_lead, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        shooterMotorright.configure(config_folllow, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        SparkMaxConfig config_follow= new SparkMaxConfig();
 
+        config_lead.closedLoop
+                .p(RobotMap.KP)
+                .i(RobotMap.KI)
+                .d(RobotMap.KD);
 
+        shooterMotorMain.configure(config_lead,ResetMode.kResetSafeParameters,PersistMode.kPersistParameters);
+        config_follow.follow(shooterMotorMain,true);
+        shooterMotorFollow.configure(config_follow, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        speedEncoder=shooterMotorMain.getEncoder();
     }
+
+    public void setWheelVoltage (int pow){
+        shooterMotorMain.setVoltage(pow);
+    }
+
+
 }
