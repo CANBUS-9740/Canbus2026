253ebc8c610fa7503a359fdc8c27b79add9678ab
itay <itaykaikov@gmail.com>
2026-01-22 20:23:28 +0200

    Added Intake and Storage - hasn't finished the IntakeArmSystem - finished the 3 commands for storage , requires a check

--- src/main/java/frc/robot/subsystems/ClimbSystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSystem.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
@@ -12,12 +16,20 @@ public class ClimbSystem extends SubsystemBase {
     SparkMax motor2;
     private final DigitalInput switchSensor1;
     private final DigitalInput switchSensor2;
+    SparkMaxConfig config;
 
     public ClimbSystem() {
         motor1 = new SparkMax(RobotMap.MOTOR1_ID, SparkLowLevel.MotorType.kBrushless);
         motor2 = new SparkMax(RobotMap.MOTOR2_ID, SparkLowLevel.MotorType.kBrushless);
         switchSensor1 = new DigitalInput(RobotMap.SWITCH_SENSOR1_ID);
         switchSensor2 = new DigitalInput(RobotMap.SWITCH_SENSOR2_ID);
+        config = new SparkMaxConfig();
+        config.closedLoop.pid(0,0,0)
+                .feedbackSensor(FeedbackSensor.kAbsoluteEncoder);
+        motor1.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        motor2.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+
+
     }
     public void moveMotor1(double speed){
         motor1.set(speed);
