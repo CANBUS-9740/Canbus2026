c3df6a54c60f86aaa94790fe52a1796f74a312c3
itay <itaykaikov@gmail.com>
2026-01-22 21:09:08 +0200

    .

--- src/main/java/frc/robot/commands/ClimbOpenLeftAndRightArmCommand.java
+++ src/main/java/frc/robot/commands/ClimbOpenLeftAndRightArmCommand.java
@@ -4,11 +4,13 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotMap;
 import frc.robot.subsystems.ClimbSystem;
 
-public class ClimbMoveLeftArmCommand extends Command {
+public class ClimbOpenLeftAndRightArmCommand extends Command {
     private final ClimbSystem climbSystem;
+    private final double targetPositionDegrees;
 
-    public ClimbMoveLeftArmCommand(ClimbSystem climbSystem){
+    public ClimbOpenLeftAndRightArmCommand(ClimbSystem climbSystem, double targetPositionDegrees){
         this.climbSystem = climbSystem;
+        this.targetPositionDegrees = targetPositionDegrees;
         addRequirements(climbSystem);
     }
 
@@ -16,6 +18,8 @@ public class ClimbMoveLeftArmCommand extends Command {
     @Override
     public void initialize() {
         climbSystem.moveMotor1(RobotMap.MOTOR1_SPEED);
+        climbSystem.moveMotor2(RobotMap.MOTOR2_SPEED);
+        climbSystem.setTargetPosition(targetPositionDegrees);
     }
 
     @Override
@@ -26,10 +30,11 @@ public class ClimbMoveLeftArmCommand extends Command {
     @Override
     public void end(boolean interrupted) {
         climbSystem.stop1();
+        climbSystem.stop2();
     }
 
     @Override
     public boolean isFinished() {
-        return climbSystem.isSwitch1Pressed();
+        return climbSystem.isSwitch1Pressed()&& climbSystem.isSwitch2Pressed();
     }
 }
