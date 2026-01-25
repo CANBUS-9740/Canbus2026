483ebb8dcbd2891184620409d75802756458a867
itay <itaykaikov@gmail.com>
2026-01-25 18:54:24 +0200

    .

--- src/main/java/frc/robot/subsystems/ClimbSystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSystem.java
@@ -1,12 +1,14 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
@@ -17,6 +19,7 @@ public class ClimbSystem extends SubsystemBase {
     private final SparkMax rightMotor;
     private final DigitalInput bottomSwitchLeft;
     private final DigitalInput bottomSwitchRight;
+    private final RelativeEncoder encoder;
 
 
     public ClimbSystem() {
@@ -24,6 +27,7 @@ public class ClimbSystem extends SubsystemBase {
         rightMotor = new SparkMax(RobotMap.CLIMB_RIGHT_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
         bottomSwitchLeft = new DigitalInput(RobotMap.CLIMB_BOTTOM_SWITCH_LEFT_SENSOR_ID);
         bottomSwitchRight = new DigitalInput(RobotMap.CLIMB_BOTTOM_SWITCH_RIGHT_SENSOR_ID);
+
         SparkMaxConfig config = new SparkMaxConfig();
         config.closedLoop.pid(0,0,0)
                 .feedbackSensor(FeedbackSensor.kAbsoluteEncoder);
@@ -49,6 +53,9 @@ public class ClimbSystem extends SubsystemBase {
         rightMotor.getClosedLoopController().setSetpoint(positionMeters/360, SparkBase.ControlType.kPosition);
 
 
+    }
+    public boolean isAtTarget(double targetPosition) {
+        return MathUtil(targetPosition,)
     }
     public boolean isBottomSwitchLeftPressed() {
         return bottomSwitchLeft.get();
