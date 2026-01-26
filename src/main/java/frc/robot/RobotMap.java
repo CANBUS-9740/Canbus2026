1b7a4bdb1dd9c2b3f12e10dcd0c1eb7bf3cd7ac7
itaykaikov <itaykaikov@gmail.com>
2026-01-26 18:00:08 +0200

    IntakeItay (#54)
    Added intake and storage

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,7 +2,21 @@ package frc.robot;
 
 public class RobotMap {
 
-    private RobotMap() {}
+     public static final double COLLECTOR_SPEED = 0.5;
+     public static final double STORAGE_GENERAL_ROLLERS_FORWARD_HIGH_SPEED = 0.5;
+    public static final double STORAGE_FEED_ROLLERS_FORWARD_HIGH_SPEED = 0.5;
+    public static final double STORAGE_FEED_ROLLERS_FORWARD_LOW_SPEED = 0.2;
+    public static final double STORAGE_GENERAL_ROLLERS_FORWARD_LOW_SPEED = 0.2;
+    public static final double STORAGE_GENERAL_ROLLERS_BACKWARDS_LOW_SPEED = -0.2;
+    public static final double STORAGE_FEED_ROLLERS_BACKWARDS_LOW_SPEED = -0.2;
+    public static final double TOLERANCE_ARM_SPEED = 0;
+    public static final int COLLECTOR_MOTOR_ID = 0;
+    public static final int INTAKE_ARM_MOTOR_ID = 0;
+    public static final int STORAGR_MOTOR1_ID = 0;
+    public static final int STORAGE_MOTOR2_ID = 0;
+    public static final int STORAGD_IRSENSOR1_ID = 0;
+    public static final int STORAGE_IRSENSOR2_ID = 0;
+
 
     // add constants here
     // public static final type NAME = value;
---
+++ src/main/java/frc/robot/subsystems/StorageSystem.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
+public class StorageSystem extends SubsystemBase {
+
+    private final SparkMax generalRollers;
+    private final SparkMax feedRollers;
+    private final DigitalInput irSensor1;
+    private final DigitalInput irSensor2;
+
+
+
+    public StorageSystem() {
+        generalRollers = new SparkMax(RobotMap.STORAGR_MOTOR1_ID, SparkLowLevel.MotorType.kBrushless);
+        feedRollers = new SparkMax(RobotMap.STORAGE_MOTOR2_ID, SparkLowLevel.MotorType.kBrushless);
+        irSensor1 = new DigitalInput(RobotMap.STORAGD_IRSENSOR1_ID);
+        irSensor2 = new DigitalInput(RobotMap.STORAGE_IRSENSOR2_ID);
+        SparkMaxConfig config = new SparkMaxConfig();
+        generalRollers.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        feedRollers.configure(config,ResetMode.kNoResetSafeParameters,PersistMode.kNoPersistParameters);
+    }
+
+    public boolean atLeast1Ball(){
+        return irSensor1.get() || irSensor2.get();
+    }
+    public boolean isFull(){
+        // should be implemented with the limelight
+        return false;
+    }
+
+
+    public void moveGeneralRollers(double speed) {
+        generalRollers.set(speed);
+    }
+    public void moveFeedRollers(double speed) {
+        feedRollers.set(speed);
+    }
+    public void stopMotors() {
+        generalRollers.stopMotor();
+        feedRollers.stopMotor();
+    }
+    public void periodic() {
+
+    }
+}
